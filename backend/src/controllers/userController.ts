270691f2cad766cd0f8705c35a27bfb1b5d4a16f
Sean Coley <github@seancoley.me>
2025-07-28 19:25:11 +1200

    chore: remove unused Prisma import in userController (fix TS6133)

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { z } from "zod";
-import { PrismaClient, Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
